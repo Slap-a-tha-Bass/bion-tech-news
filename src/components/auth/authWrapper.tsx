7c7a3ba83f6e73d4a6f42fc3bdeeab0e20be6efd
Corey DeLoach <cdeloach16@gmail.com>
2022-04-20 22:12:08 -0500

    more oauth contd

--- src/components/auth/authWrapper.tsx
+++ src/components/auth/authWrapper.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { useRouter } from "next/router";
 import PrivateRoute from "./privateRoute";
 
-const authRoutes = ["/", "/reddit-news", "/news-api-org", "/news-data-io"];
+const authRoutes = ["/reddit-news", "/news-api-org", "/news-data-io"];
 
 export default function AuthWrapper({
   children,
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -6,7 +6,9 @@ export default function Login() {
       <p className="text-right text-xs text-gray-500">Not signed in</p>
       <button
         className="p-2 rounded-lg text-md text-sky-300 shadow-lg shadow-sky-400"
-        onClick={() => signIn()}
+        onClick={() => signIn("github", {
+          callbackUrl: "http://bion-tech.vercel.app/",
+        })}
       >
         Sign in
       </button>
