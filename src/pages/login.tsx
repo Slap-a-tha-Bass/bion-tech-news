d1dc0b61dc2e249991b1c013ef166dc4f1d1da53
Corey DeLoach <cdeloach16@gmail.com>
2022-04-20 20:40:03 -0500

    callback URL added to sign in...troubleshooting contd

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -6,7 +6,11 @@ export default function Login() {
       <p className="text-right text-xs text-gray-500">Not signed in</p>
       <button
         className="p-2 rounded-lg text-md text-sky-300 shadow-lg shadow-sky-400"
-        onClick={() => signIn("github")}
+        onClick={() =>
+          signIn("github", {
+            callbackUrl: "https://bion-tech.vercel.app/",
+          })
+        }
       >
         Sign in
       </button>
