7d163f8c9c1af1da7d1f095f0b28d3daca97c881
Corey DeLoach <cdeloach16@gmail.com>
2022-04-21 09:37:51 -0500

    testing JSON parse for localstorage

--- src/pages/mylinks.tsx
+++ src/pages/mylinks.tsx
@@ -1,20 +1,18 @@
 import { useEffect, useState } from "react";
 
 interface ILink {
-    title: string;
-    url: string;
+  title: string;
+  url: string;
 }
 
 export default function MyLinks() {
-    const [myLinks, setMyLinks] = useState('');
-    useEffect(() => {
-        const links = localStorage.getItem("links");
-        if (links) {
-            setMyLinks(links);
-        }
-        console.log({myLinks});
-    }, [myLinks]);
-    return (
-        <h1>{myLinks}</h1>
-    )
+  const [myLinks, setMyLinks] = useState("");
+  useEffect(() => {
+    const links = localStorage.getItem("links");
+    if (links) {
+      const parsedLinks = JSON.parse(links);
+      setMyLinks(parsedLinks);
+    }
+  }, [myLinks]);
+  return <h1>{myLinks}</h1>;
 }
